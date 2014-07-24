6fef9d3bffb7dc4aed44baded497be9d896ad436
marina <msarmengol@gmail.com>
2014-07-24 14:31:03 +0200

    second and last version

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My version for Assignment 2 -R Programming course
 
-## This function allows to create an object type matrix with four methods for manipuling it.
+## This function allows to create an object which handles with matrixs that can be inverted.
+## It returns a public list  of four methods. 
 
-makeCacheMatrix <- function(x = matrix()) {
-        mat_inv <- NULL
-        set <- function(y) {
+makeCacheMatrix <- function(x=matrix()) {
+        mat_inv <- NULL                                       # Cleanning environment
+        set <- function(y) {                                  # Establish new data
                 x <<- y
                 mat_inv <<- NULL
         }
-        get <- function() x
-        setsolve <- function(x_inv) mat_inv <<- x_inv
-        getsolve <- function() mat_inv
-        list(set = set, get = get,
-             setsolve = setsolve,
-             getsolve = getsolve)
+        get <- function() x                                   # Just return what is gotten
+        setInverse <- function(x_inv) mat_inv <<- x_inv       # Overwrite in the global envirnment tha value is given
+        getInverse <- function() mat_inv                      # Just return what is gotten 
+        list(set = set, get = get,                            # Return public methods
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
+## This funciont carries on with the asked bussiness logic.
+## Obtein the inverse matrix, if the result existes, get it from the cahe instead of calculating.
 
-## This funciont carries on with the bussiness logic: if the result existes, get it from the cahe instead of calculating.
+cacheSolve <- function(x) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-
-        mat_inv <- x$getsolve()
-        if(!is.null(mat_inv)& x$mat_old == x$get()) {
+        mat_inv <- x$getInverse()                             # Gets the result stored in globla environment
+        if(!is.null(mat_inv)) {                               # Checks if it's necesary to calculate
                 message("getting cached data")
-                return(mat_inv)
+                return(mat_inv)                               # Return the value stored in the global environment
         }
-        x$mat_old <- x$get()
-        mat_inv  <- x$setsolve(solve(x$get()))
-        
+        mat_inv  <- x$setInverse(solve(x$get()))              # Calculate the inverse of the given matrix                  
+        return(mat_inv)
 }
